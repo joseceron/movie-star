2aa59a5e8922e6482d68a9af4c8bee3e94bd441d
Jose Ceron <joseluisceron13@gmail.com>
2022-10-10 15:20:39 -0500

    fix: integration test adding token to its header

--- server/src/infrastructure/driving-adapters/api-rest/controllers/movie/movie.test.ts
+++ server/src/infrastructure/driving-adapters/api-rest/controllers/movie/movie.test.ts
@@ -6,73 +6,110 @@ const server = new Server(PORT)
 const app = server._app
 
 let movieId: String = ''
+const token: string = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3QzQGdtYWlsLmNvbSIsImlhdCI6MTY2NTQxNjI4MX0.dNGh_kDwFvwJpMH9bU0bSNjg85CfEX727nf0JDMt3Zw'
 
-describe('Test GET/movies', () => {
+describe('Test GET /v1/movies', () => {
   test('It should response with 200 success', async () => {
     const response = await request(app)
-      .get('/movies')
+      .get('/v1/movies')
+      .set('Authorization', `Bearer ${token}`)
       .expect('Content-Type', /json/)
       .expect(200)
-
     expect(Array.isArray(response.body)).toBe(true)
   })
 })
 
-describe('Test POST /movies', () => {
+describe('Test POST /v1/movies', () => {
   const completeMovieData = {
-    title: 'movieTest',
-    year: '1900'
+    title: 'Test-2',
+    year: '2022',
+    rating: 3,
+    castAndCrew: {
+      actors: [
+        'Silvestre Stalon',
+        'ronaldo',
+        'halan'
+      ],
+      director: [
+        'Silvestre Stalon',
+        'migue cerón'
+      ]
+    },
+    genre: {
+      id: 1
+    }
   }
+
   test('It should respond with 201 created', async () => {
     const response = await request(app)
-      .post('/movies')
+      .post('/v1/movies')
+      .set('Authorization', `Bearer ${token}`)
       .send(completeMovieData)
       .expect('Content-Type', /json/)
       .expect(201)
 
     const body = response.body
     movieId = body.id
-
-    expect(Object.keys(body).length).toBe(3)
+    console.log(body)
+    expect(Object.keys(body).length).toBe(6)
     expect(body).toMatchObject(completeMovieData)
   })
 })
 
-describe('Test GET /movies:movieId', () => {
+describe('Test GET /v1/movies/movieId', () => {
   test('It should get movie by movieId', async () => {
     const response = await request(app)
-      .get(`/movies/${movieId}`)
+      .get(`/v1/movies/${movieId}`)
+      .set('Authorization', `Bearer ${token}`)
       .expect('Content-Type', /json/)
       .expect(200)
 
     const body = response.body
     const keysSize = Object.keys(body).length
-    expect(keysSize).toBe(3)
+    expect(keysSize).toBe(6)
     expect(body.id).toBe(movieId)
   })
 })
 
 describe('Test PUT /movies', () => {
   const movieUpdateData = {
-    title: 'movieTestUpdated!',
-    year: '1950'
+    title: 'Test-Updated',
+    year: '2022',
+    rating: 3,
+    castAndCrew: {
+      actors: [
+        'Silvestre Stalon',
+        'ronaldo',
+        'halan'
+      ],
+      director: [
+        'Silvestre Stalon',
+        'migue cerón'
+      ]
+    },
+    genre: {
+      id: 1
+    }
   }
   test('It should update the movie', async () => {
     const response = await request(app)
-      .put(`/movies/${movieId}`)
+      .put(`/v1/movies/${movieId}`)
+      .set('Authorization', `Bearer ${token}`)
       .send(movieUpdateData)
       .expect('Content-Type', /json/)
       .expect(200)
 
     const body = response.body
-    expect(Object.keys(body).length).toBe(3)
+    console.log(body)
+    expect(Object.keys(body).length).toBe(6)
   })
 })
 
-describe('Test DELETE /movies', () => {
+describe('Test DELETE /v1/movies', () => {
   test('It should respond with 204', async () => {
     const response = await request(app)
-      .delete(`/movies/${movieId}`)
+      .delete(`/v1/movies/${movieId}`)
+      .set('Authorization', `Bearer ${token}`)
       .expect(204)
     expect(response.noContent).toBe(true)
   })
